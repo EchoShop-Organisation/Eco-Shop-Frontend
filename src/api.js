0e75bc8eab39e93c017308416714ba39d578cb86
Munjwok James Alala <habib.munji@gmail.com>
2025-03-09 00:32:21 +0300

    created api utility

--- src/api.js
+++ src/api.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api/users";
+// Separate API URLs
+const USER_API_BASE_URL = "http://127.0.0.1:8000/api/users";
+const PRODUCT_API_BASE_URL = "http://127.0.0.1:8000/api/products";
 
+// User API Functions
 export const registerUser = async (userData) => {
-  return axios.post(`${API_BASE_URL}/register/`, userData);
+  return axios.post(`${USER_API_BASE_URL}/register/`, userData);
 };
 
 export const loginUser = async (credentials) => {
-  return axios.post(`${API_BASE_URL}/login/`, credentials);
+  return axios.post(`${USER_API_BASE_URL}/login/`, credentials);
 };
 
 export const getUserProfile = async (token) => {
-  return axios.get(`${API_BASE_URL}/profile/`, {
+  return axios.get(`${USER_API_BASE_URL}/profile/`, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
+
+// Product API Function
+export const getProducts = async () => {
+  return axios.get(PRODUCT_API_BASE_URL);
 };

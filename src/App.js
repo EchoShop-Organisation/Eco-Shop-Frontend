fc823fc8749e3babf2c0a551f666199acba94937
Munjwok James Alala <habib.munji@gmail.com>
2025-03-09 00:35:35 +0300

    Add Route for Product Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
+import Products from "./pages/Products";
+
+
 
 function App() {
   return (
@@ -16,6 +19,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        <Route path="/products" element={<Products />} />
+
       </Routes>
     </Router>
   );
